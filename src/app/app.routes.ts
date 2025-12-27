8fd3f735172aa83a74560dfa50752983fb2a0c87
Jader <jadergav@hotmail.com>
2025-12-27 15:56:38 -0500

    Adiciona Github Actions para el despligue en Github Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,52 @@
+// import { Routes } from '@angular/router';
+// import { DetalleProyecto } from './detalle-proyecto/detalle-proyecto';
+// import { Home } from './home/home';  // Ajusta la ruta según donde se genere
+
+// export const routes: Routes = [
+//   {
+//     path: '',
+//     component: Home  // Muestra el componente Home en la ruta raíz
+//   },
+//   {
+//     path: 'detalle-proyecto',
+//     // component: DetalleProyecto
+//     loadComponent: () => import('./detalle-proyecto/detalle-proyecto')
+//       .then(m => m.DetalleProyecto)
+//   },
+//   {
+//     path: 'detalle-proyecto/:id',
+//     // component: DetalleProyecto,
+//     loadComponent: () => import('./detalle-proyecto/detalle-proyecto')
+//       .then(m => m.DetalleProyecto)
+//   },
+//   {
+//     path: '**',  // Ruta comodín para 404
+//     redirectTo: ''
+//   }
+// ];
+
 import { Routes } from '@angular/router';
-import { DetalleProyecto } from './detalle-proyecto/detalle-proyecto';
-import { Home } from './home/home';  // Ajusta la ruta según donde se genere
+import { Home } from './home/home';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Home  // Muestra el componente Home en la ruta raíz
+    component: Home
   },
   {
     path: 'detalle-proyecto',
-    component: DetalleProyecto
-    // redirectTo: ''
+    loadComponent: () =>
+      import('./detalle-proyecto/detalle-proyecto')
+        .then(m => m.DetalleProyecto),
   },
   {
     path: 'detalle-proyecto/:id',
-    component: DetalleProyecto
+    loadComponent: () =>
+      import('./detalle-proyecto/detalle-proyecto')
+        .then(m => m.DetalleProyecto),
   },
   {
-    path: '**',  // Ruta comodín para 404
+    path: '**',
     redirectTo: ''
   }
 ];
